08ae3008b9e5227515d93537f51f6f3817134110
DebajyotiGoswami <goswamidiary@gmail.com>
2022-05-16 00:04:09 +0530

    bar chart display created by showing characters multiple times rather count only

--- chapter_1/poorManBarChart.py
+++ chapter_1/poorManBarChart.py
@@ -2,13 +2,18 @@
 program that takes a sentence as input and returns a bar chart type
 count of characters from the sentence
 '''
+import pprint
+
 def show_chart(chart):
     '''
     print the dictionary of character wise count as a bar chart
     attribute -- dictionary
     return -- None
     '''
-    pass
+    for key, value in chart.items():
+        chart[key] = list(key * value)
+
+    pprint.pprint(chart)
 
 def create_chart(sentence):
     '''
@@ -16,7 +21,15 @@ def create_chart(sentence):
     attribute -- strings
     return -- dictionary
     '''
-    return None
+    letters = list('abcdefghijklmnopqrstuvwxyz')
+    chart = {}
+
+    for character in sentence:
+        if character in letters:
+            chart.setdefault(character, 0)
+            chart[character] += 1
+
+    return chart
 
 def main():
     '''
