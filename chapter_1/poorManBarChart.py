5ecf755d57b4cab38e614ed1c3ebdc2450f9a565
DebajyotiGoswami <goswamidiary@gmail.com>
2022-05-15 23:46:08 +0530

    program to print character count of a sentence
    takes a sentence as input from user
    and print the count of its character like a bar chart

---
+++ chapter_1/poorManBarChart.py
@@ -0,0 +1,30 @@
+'''
+program that takes a sentence as input and returns a bar chart type
+count of characters from the sentence
+'''
+def show_chart(chart):
+    '''
+    print the dictionary of character wise count as a bar chart
+    attribute -- dictionary
+    return -- None
+    '''
+    pass
+
+def create_chart(sentence):
+    '''
+    takes an strings as input and returns a character wise count
+    attribute -- strings
+    return -- dictionary
+    '''
+    return None
+
+def main():
+    '''
+    main function. our program starts here
+    '''
+    sentence = input("Enter your sentence : ")
+    chart = create_chart(sentence)
+    show_chart(chart)
+
+if __name__ == '__main__':
+    main()
