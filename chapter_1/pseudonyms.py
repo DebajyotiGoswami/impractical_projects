b539b6a41fbf4a53b3eb097196c6c0a918d219c7
DebajyotiGoswami <goswamidiary@gmail.com>
2022-05-15 12:54:41 +0530

    pseudonyms.py of chapter 1 is executed successfully

---
+++ chapter_1/pseudonyms.py
@@ -0,0 +1,63 @@
+'''
+a simple program to write funny names from two diffrent lists
+'''
+
+import sys
+import random
+
+def get_names():
+    '''
+    initialise two lists of first names and last names
+    parameter -- None
+    Return -- two tuples
+    '''
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+         "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+         'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+         'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+         'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+         'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+         'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+         'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
+         '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
+         'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
+         'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+         "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+         'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+         'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+         "Winston 'Jazz Hands'", 'Worms')
+        
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+        'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+        'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+        'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+        'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+        'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+        'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+        'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+        'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+        'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+        'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+        'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+        'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+        'Woolysocks')
+    return first, last
+
+def main():
+    '''
+    main function. our program starts here
+    '''
+    first, last = get_names()
+    while True:
+        #program will continue until user stops it by choice
+        firstName = random.choice(first)
+        lastName = random.choice(last)
+        print("{} {}".format(firstName, lastName), file= sys.stderr)
+        choice= input("Want to continue : Y / N ")
+        if choice.lower() ==  'n':
+            break
+
+    print("Game ends")
+
+if __name__ == '__main__':
+    main()
