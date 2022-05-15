c2aaaa3f6f1da2b7636befcd8e2497e659144885
DebajyotiGoswami <goswamidiary@gmail.com>
2022-05-15 15:18:24 +0530

    pigLatin.py changed as per pylint conventions

--- chapter_1/pigLatin.py
+++ chapter_1/pigLatin.py
@@ -1,18 +1,23 @@
-import sys
-
 '''
 program to generate pigLatin equivalent of a user input sentence
 
 if the first letter of each word is consonant :
     move that consonant to the end
     and then add "ay" to the end of the word
-    
+
 if the first letter of each word is vowel:
     simply add "way" to the end of the word
 '''
+import sys
+
 def pigLatin(sentence):
+    '''
+    convert a sentence to its pigLatin counterpart
+    parameter -- string
+    return -- string
+    '''
     words = sentence.split()
-    pig_latin= ''
+    pig_latin = ''
     for word in words:
         if word[0].lower() in list('aeiou'):
             pig_latin += word + 'way'
@@ -27,7 +32,7 @@ def main():
     '''
     sentence = input("Enter your sentence : ")
     pig_latin = pigLatin(sentence)
-    print(pig_latin, file= sys.stderr)
+    print(pig_latin, file=sys.stderr)
 
 if __name__ == '__main__':
     main()
