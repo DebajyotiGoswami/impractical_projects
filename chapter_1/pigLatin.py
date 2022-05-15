802ef49bd971b1929c346b1176b3828416b5e1ab
DebajyotiGoswami <goswamidiary@gmail.com>
2022-05-15 15:13:19 +0530

    pigLatin.py created successfully but not working properly with dots and commas

---
+++ chapter_1/pigLatin.py
@@ -0,0 +1,33 @@
+import sys
+
+'''
+program to generate pigLatin equivalent of a user input sentence
+
+if the first letter of each word is consonant :
+    move that consonant to the end
+    and then add "ay" to the end of the word
+    
+if the first letter of each word is vowel:
+    simply add "way" to the end of the word
+'''
+def pigLatin(sentence):
+    words = sentence.split()
+    pig_latin= ''
+    for word in words:
+        if word[0].lower() in list('aeiou'):
+            pig_latin += word + 'way'
+        else:
+            pig_latin += word[1:] + word[0]+ 'ay'
+        pig_latin += ' '
+    return pig_latin
+
+def main():
+    '''
+    main function. program starts here
+    '''
+    sentence = input("Enter your sentence : ")
+    pig_latin = pigLatin(sentence)
+    print(pig_latin, file= sys.stderr)
+
+if __name__ == '__main__':
+    main()
